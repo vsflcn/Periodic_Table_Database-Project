ef7111c194f409cf7046a380ed24daf63f04b4af
Volha Sakalouskaya <falcoooneee@proton.me>
2024-10-18 13:09:49 +0000

    feat: add main program function

--- element.sh
+++ element.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+
+MAIN_PROGRAM() {
+  if [[ -z $1 ]]
+  then
+    echo "Please provide an element as an argument."
+  else
+    PRINT_ELEMENT $1
+  fi
+}
